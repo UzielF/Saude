769e715c43ccbd47135ca498eaca0a054c79e375
UzielF <dev+github@uzielf.com>
2025-07-10 11:11:57 -0300

    Desenvolvimento pela manhã no hospital

--- src/js/dom.ts
+++ src/js/dom.ts
@@ -5,5 +5,16 @@ export const DOM =
         let elementos = document.getElementsByClassName(classe);
         if (elementos.length !== 1) return undefined;
         return <HTMLElement>elementos[0];
+    },
+    Criar: (tipo: string, classes: Array<string> | string | undefined = undefined, id: string | undefined = undefined) =>
+    {
+        let elemento = document.createElement(tipo);
+
+        if (classes instanceof Array) elemento.classList.add(...classes);
+        else if (typeof classes === "string") elemento.classList.add(classes);
+
+        if (typeof id === "string") elemento.id = id;
+
+        return elemento;
     }
 } as const;
